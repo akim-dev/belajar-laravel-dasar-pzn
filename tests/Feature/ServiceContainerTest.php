f54cf2a15d2f0f71a157987c92055cd92d4b11d6
musta.akim <musta.akim@gmail.com>
2023-02-22 08:46:31 +0700

    test3

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Data\Foo;
+use App\Data\Person;
+
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -20,4 +22,51 @@ class ServiceContainerTest extends TestCase
     self::assertNotSame($foo, $foo1);
 
    }
+
+   public function testBind() {
+    // $person = $this->app->make(Person::class);
+
+    $this->app->bind(Person::class,function($app){
+        return new Person ("Akim", "John");
+    });
+    $person = $this->app->make(Person::class);
+    $person1 = $this->app->make(Person::class);
+
+    // self::assertNotNull($person);
+    self::assertEquals('Akim', $person->firstName);
+    self::assertEquals('John', $person->lastName);
+    self::assertNotSame($person1, $person);
+   }
+
+
+   public function testSingleton() {
+    // $person = $this->app->make(Person::class);
+
+    $this->app->singleton(Person::class,function($app){
+        return new Person ("Akim", "John"   );
+    });
+    $person = $this->app->make(Person::class);
+    $person1 = $this->app->make(Person::class);
+
+    self::assertNotNull($person);
+    self::assertEquals('John', $person->lastName);
+    self::assertSame($person1, $person);
+   }
+
+   public function testInstance() {
+    // $person = $this->app->make(Person::class);
+
+    $this->app->instance(Person::class,function($app){
+        return new Person ("Akim", "John"   );
+    });
+    $person = $this->app->make(Person::class);//$person
+    $person1 = $this->app->make(Person::class);//$person
+    $person2 = $this->app->make(Person::class);//$person
+    $person3 = $this->app->make(Person::class);//$person
+
+    self::assertNotNull($person);
+    self::assertEquals('John', $person->lastName);
+    self::assertSame($person1, $person);
+   }
+
 }
